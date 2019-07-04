d40181d9d563f3cc51f24bceb4217a656cb5c1cb
shubhammadhwal <49407815+shubhammadhwal@users.noreply.github.com>
2019-07-04 12:12:33 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,12 @@
+class shubham(object):
+    def __init__(self,l,b):
+        self.l=l;
+        self.b=b;
+
+    def bs(self, bl):
+        self.b=self.b+bl
+        return(self.b)
+    
+s=shubham(10,20)
+
+    
